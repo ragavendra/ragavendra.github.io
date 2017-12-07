b2596a3f2e40e68a7b45c039155c5e24823d9afd
ragavendra <ragavendra.bn@gmail.com>
2017-12-07 12:18:32 -0800

    tried user save message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+	
 	attr_accessor	:name, :email
+
   def new
+		@user = User.new
   end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+
+			#find if user save success
+			@message = "User created successfully" if User.find_by_email(@user.email)
+			
+			redirect_to	@user
+		else
+			render 'new'
+		end
+	end
+
+  def show
+		@user = User.find(params[:id])
+		
+			@message = "User created successfully" if User.find_by_email(@user.email)
+		#return message for success save
+		return @user, @message
+  end
+
+	private
+
+	def user_params
+		params[:id]
+		params.require(:user).permit(:name, :email, :password,
+																 :password_confirmation)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new', as: 'signup'
 
   get 'static_pages/home'
   get '/help', to: 'static_pages#help'
@@ -7,6 +6,9 @@ Rails.application.routes.draw do
   get '/blog', to: 'static_pages#blog'
   get '/contact', to: 'static_pages#contact'
   get '/login', to: 'static_pages#login'
+  get '/signup', to: 'users#new', as: 'signup'
+
+	resources	:users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'static_pages#home'
