4efd6bb1028b3f99f43575eb59f7b5630cf51cb1
ragavendra <ragavendra.bn@gmail.com>
2017-12-21 10:18:07 -0800

    moving to slim

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,17 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes (user_params)
+			flash[:success] = "#{@user.name} profile updated succesfully"
+			redirect_to @user
+
+		else
+			render 'edit'
+		end
+	end
+
   def show
 		@user = User.find(params[:id])
 		
